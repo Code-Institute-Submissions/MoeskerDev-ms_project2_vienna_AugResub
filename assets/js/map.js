c51283766446aa87f177e7eb274adc88e9934979
MoeskerDev <mgmoesker@gmail.com>
2021-04-17 21:34:58 +0000

    added two sleep markers with name, information and website in infowindow and styled form for mobile with a full width

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,6 @@
 //The focus of the map is always Vienna via it's lat and lng position
 
-function initMap() {
+function findSleep() {
 
     const properties = {
         lat: 48.2082,
@@ -10,14 +10,105 @@ function initMap() {
     const mapProp = {
 
         center: properties,
-        zoom: 10,
+        zoom: 12,
     };
     //Creates a new map inside the div with id map and it calls the properties to know how to render the map
     const map = new google.maps.Map(document.getElementById("map"), mapProp);
+
+    //Add markers regarding my recommendations for sleeping in Vienna
+    //function findSleep() {
+    const myMarks = [{
+            "lat": 48.196790,
+            "lng": 16.360930,
+            "name": "Wombat's hostel",
+            "information": `Wombat’s The City Hostel Vienna is located right next to the Naschmarkt. Just head out the front door to browse the food stalls and also the flea market on Saturday! We’re easy to reach: the metro station Kettenbrückengasse (U4) is located just a few minutes away. Feel free to join in on different group activities and make new friends in our cheerful WomBar. You can share a room or even stay in a private room.`,
+            "website": "https://www.wombats-hostels.com/vienna/the-naschmarkt/"
+        },
+        {
+            "lat": 48.204140,
+            "lng": 16.342310,
+            "name": "Hotel Sans Souci",
+            "information": `The Sans Souci Wien, a boutique hotel, is the answer to every unspoken desire for an urban retreat located right at the heart of Vienna’s cultural and artistic city centre. Directly adjacent to the Museums Quarter Vienna, it is a haven of relaxation with a multitude of delights for the senses, and a place of art.`,
+            "website": "https://www.sanssouci-wien.com/en/"
+        }
+    ];
+
+    var InfoObj = [];
+
+    for (let i = 0; i < myMarks.length; i++) {
+        let contentString = `<h3>${myMarks[i].name}</h3><p>${myMarks[i].information}</p>${myMarks[i].website}`
+
+        const marker = new google.maps.Marker({
+            position: new google.maps.LatLng(myMarks[i].lat, myMarks[i].lng),
+            map: map,
+            title: myMarks[i].name,
+            animation: google.maps.Animation.DROP,
+        });
+        //Set up infowindow
+        const infoWindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 500
+        });
+        //Add a click listener to the marker
+        marker.addListener("click", function () {
+            //Close other windows
+            closeOtherInfo();
+            //Create new window
+            infoWindow.open(map, marker);
+            InfoObj[0] = infowindow;
+        });
+    }
+    //This function clears old information
+    function closeOtherInfo() {
+        if (InfoObj.length > 0) {
+            InfoObj[0].set("marker", null);
+            InfoObj[0].close();
+            InfoObj[0].length = 0;
+        }
+    }
 }
 
 //Calling the map
-initMap();
+findSleep();
+
+//   }
+
+
+
+
+
+
+//Create functions for each button
+//Add markers
+//function findSleep() {
+//   const myMarks = [{
+//          "lat": 48.196790,
+//          "lng": 16.360930,
+//          "name": "Wombat's hostel"
+//      },
+//      {
+//          "lat": 48.204140,
+//          "lng": 16.342310,
+//          "name": "Hotel Sans Souci"
+//      }
+//  ];
+//  for (let i = 0; i < myMarks.length; i++) {
+//      const marker = new google.maps.Marker({
+//          position: new google.maps.LatLng(myMarks[i].lat, myMarks[i].lng),
+//          map: map,
+//         title: myMarks[i].name
+//      });
+//marker.setMap(map);
+//   }
+//}
+
+
+
+
+
+
+
+
 
 
 
