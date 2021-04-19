4e8aa52deae8b3e1baa07687669587d7d5df2d14
MoeskerDev <mgmoesker@gmail.com>
2021-04-19 18:35:34 +0000

    fixed the form code regarding an alert message from my function not showing

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ function initMap() {
         lng: 16.3738
     };
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
+        zoom: 12,
         center: myLatLng,
     });
     new google.maps.Marker({
@@ -18,14 +18,7 @@ function initMap() {
     });
 }
 
-
-
-
-
-
-
-
-
+//Change colour of button after being clicked
 $(".buttons").on("click", function () {
     let allButtonsSelector = ".btn";
     let thisButtonsSelector = "." + this.id;
@@ -33,13 +26,23 @@ $(".buttons").on("click", function () {
     $(thisButtonsSelector).addClass("btn-danger");
 })
 
+//Function that will show an alert depending on if all fields are filled out or not plus in case of another error
+let submitBtn = document.getElementById('submit-btn');
 
+submitBtn.addEventListener('click', () => {
+    submitClick();
+})
 
-function submitClick() {
-    if ("name" && "email" && "question" !== null) {
+function submitClick(e) {
+    console.log("Testing testing 1 2 3");
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let question = document.getElementById("question").value;
+
+    if (name !== "" && email !== "" && question !== "") {
         alert("Thank you for submitting your question(s)!");
-    } else if ("name" || "email" || "question" == null) {
-        alert("Please fill out all fields on the form.");
+    } else if (name == "" || email == "" || question == "") {
+        alert("Please fill out all fields of the form.");
     } else
         alert("Sorry, something went wrong.. Please try again.");
 
