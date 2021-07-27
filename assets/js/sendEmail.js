36b230e4c170e8979be92f9a0d0b6277a1cfe4af
MoeskerDev <mgmoesker@gmail.com>
2021-07-27 13:13:43 +0000

    Removed the code for EmailJS in js file and commented out the code in the HTML file

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,9 +1,13 @@
+/*copied and adapted from the CI tutorial. to send a certain template from EmailJS
+and link it to the form names on the website*/
 function sendEmail(contact) {
     emailjs.send("service_3821dh8", "template_zqobdjg", {
-            "from_name": contact.name.value,
-            "from_email": contact.email.value,
-            "questions": contact.questions.value,
-        })
+                "from_name": contact.name.value,
+                "from_email": contact.email.value,
+                "questions": contact.questions.value,
+            },
+            ("user_c9Clrkl69n1cSk0s5qSG1")
+        )
         .then(
             function (response) {
                 console.log("SUCCES", response);
@@ -12,3 +16,13 @@ function sendEmail(contact) {
                 console.log("FAILED", error);
             });
 }
+
+
+/*added to remove the unused function sendEmail. it takes the id from
+the HTML element that, when it is clicked on, triggers the sendEmail 
+function above.*/
+let btn = document.getElementById("form-btn");
+
+btn.addEventListener("click", () => {
+    sendEmail();
+});
